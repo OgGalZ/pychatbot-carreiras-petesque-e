7e323dfd69165e5f913d9a0024cd5ec8e5f24fd4
enzoc <oggalze@gmail.com>
2023-11-20 11:27:27 +0100

    utils

--- pythonproject_L1_S1/src/main.py
+++ pythonproject_L1_S1/src/main.py
@@ -1,5 +1,8 @@
 from api import functions as fn
 directory = "./speeches-20231114"
 files_names = fn.list_of_files(directory, "txt")
-table = fn.recover_names_presidents(files_names)
-fn.display_names(table)
+fn.display_names(files_names)
+
+
+#demander pour un truc utils
+#et aussi pour display je dois faire avant le recover donc comment faire un truc this.table_names_presidents = recovery(table...)

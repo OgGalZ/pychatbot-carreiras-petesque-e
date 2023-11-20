22f75716d6ea44bf2fe79c2206cd0067e66c1240
enzoc <oggalze@gmail.com>
2023-11-20 11:24:09 +0100

    test

--- pythonproject_L1_S1/src/main.py
+++ pythonproject_L1_S1/src/main.py
@@ -1,3 +1,5 @@
 from api import functions as fn
 directory = "./speeches-20231114"
 files_names = fn.list_of_files(directory, "txt")
+table = fn.recover_names_presidents(files_names)
+fn.display_names(table)

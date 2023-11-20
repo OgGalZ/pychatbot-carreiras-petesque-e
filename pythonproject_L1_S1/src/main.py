c4447feea544fb123dcd5a6558536e0bead8b8cd
oggalz <oggalze@gmail.com>
2023-11-20 00:04:59 +0000

    functions

--- pythonproject_L1_S1/src/main.py
+++ pythonproject_L1_S1/src/main.py
@@ -1,4 +1,5 @@
 from api import functions as fn
-
 directory = "./speeches-20231114"
 files_names = fn.list_of_files(directory, "txt")
+files_names = fn.recover_names_presidents(files_names)
+fn.display_names(files_names)

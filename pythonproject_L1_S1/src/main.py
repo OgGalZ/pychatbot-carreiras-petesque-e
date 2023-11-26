791e04f561aa1fad9ab5a0e3310f88182134221e
oggalz <oggalze@gmail.com>
2023-11-26 02:45:01 +0000

    tf et idf

--- pythonproject_L1_S1/src/main.py
+++ pythonproject_L1_S1/src/main.py
@@ -2,6 +2,11 @@ import functions as fn
 from api import utils
 
 directory = "cleaned"
-files_names = fn.list_of_files(directory, "txt")
-
+files_names = fn.list_of_files("speeches-20231124", "txt")
+fn.display_names(files_names)
+fn.cleaned("speeches-20231124")
+fn.remove_punctuation_character()
+for a in files_names:
+    content = utils.recover_string_file(directory, a)
+    print(fn.TF(content))
 print(fn.idf(directory))
